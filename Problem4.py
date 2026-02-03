4896ceb2296483ce521d75bf0f4bfe7c018d8241
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2026-02-02 21:52:28 -0500

    Problem 4 complete

---
+++ Problem4.py
@@ -0,0 +1,60 @@
+"""
+COMP4476 - Introduction to Cryptography
+Francesco Coniglione (1206780)
+Assignment 2 - Problem 4
+
+Reference(s): https://www.geeksforgeeks.org/dsa/primality-test-set-3-miller-rabin/
+"""
+
+import random
+
+def miller_rabin(n, k=8):
+    if n <= 1: return False
+    if n <= 3: return True
+    if n % 2 == 0: return False
+
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, d, n)
+        
+        if x == 1 or x == n - 1:
+            continue
+            
+        for _ in range(s - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+            
+    return True
+
+def test_number(n):
+    if miller_rabin(n):
+        print(f"the number {n} is a prime")
+    else:
+        print(f"the number {n} is not a prime")
+
+def find_big_primes(start, count=3):
+    print(f"Three prime numbers larger than {start} are:")
+    found = 0
+    current = start + 1
+    if current % 2 == 0: current += 1 
+    
+    while found < count:
+        if miller_rabin(current):
+            print(f"Found: {current}")
+            found += 1
+        current += 2
+
+if __name__ == "__main__":
+    val = int(input("Enter integer: "))
+    test_number(val)
+    
+    find_big_primes(10**8)
